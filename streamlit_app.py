0007a9d72ab13a63ac5eb29c8dff77ee139a18d9
vpriyathegreat <95736159+vpriyathegreat@users.noreply.github.com>
2023-09-28 20:07:50 +0530

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+
+import seaborn as sns
+
+st.title("My Streamlit App")
+
+df=pd.read_csv("air-quality-india")
+st.write(df)
+# List of required libraries
