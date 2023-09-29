7e3045cfc2ebb4f1a1cf99c24421ba3cc8187c05
vpriyathegreat <95736159+vpriyathegreat@users.noreply.github.com>
2023-09-29 21:56:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -257,7 +257,7 @@ if selected=="highest and lowest aqi analysis":
       table = pd.pivot_table(a, values = 'aqi', index ='Year',
                          columns ='Month')
       st.write(table)
-     with st.expander("See explanation"):
+      with st.expander("See explanation"):
        st.markdown("We notice that in the month of November, there's consistently high AQI because of festivals like Diwali.")
        st.markdown("Diwali is a Hindu festival that is celebrated with fireworks and bonfires. The burning of fireworks and bonfires releases large amounts of pollutants into the air.")
        st.markdown("The poor air quality in India in November 2017 had a significant impact on public health. Schools were closed in Delhi and other cities due to the poor air quality. Many people also reported experiencing respiratory problems.")
